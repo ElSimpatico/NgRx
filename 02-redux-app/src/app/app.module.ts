8eff7574d7b3173534c03519ec22e3aaf882a60f
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-09 17:36:12 +0200

    NgRx store devtools

--- 02-redux-app/src/app/app.module.ts
+++ 02-redux-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 // ngrx
 import { StoreModule } from '@ngrx/store';
 import { contadorReducer } from './contador/contador.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,11 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({contador: contadorReducer})
+    StoreModule.forRoot({contador: contadorReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retiene los ultimos 25 estados
+      logOnly: environment.production // Restringir el control del estado al entorno de pro
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
