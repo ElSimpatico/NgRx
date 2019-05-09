ad892d03c2f4b6b104bb995ff1b5fdbefe685453
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-09 17:20:32 +0200

    NgRx y contador (Acciones y Reducer)

--- 02-redux-app/src/app/app.component.ts
+++ 02-redux-app/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux-app';
+  contador: number;
+
+  constructor(private store: Store<AppState>) {
+    this.store.subscribe(state => {
+      this.contador = state.contador;
+    });
+  }
+
+  incrementar() {
+    const action = new IncrementarAction();
+    this.store.dispatch(action);
+  }
+
+  decrementar() {
+    const action = new DecrementarAction();
+    this.store.dispatch(action);
+  }
 }
