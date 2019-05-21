cca26ce944aa544eea28668f7480d91c0529835f
Aaron Velasco <tiquismiquisss@gmail.com>
2019-05-21 21:22:26 +0200

    agregar todos

--- 03-todoapp/src/app/todo/todo.reducer.ts
+++ 03-todoapp/src/app/todo/todo.reducer.ts
@@ -1,7 +1,11 @@
 import * as fromTodo from './todo.actions';
 import { Todo } from './models/todo.model';
 
-const estadoInicial: Todo[] = [];
+const todo1 = new Todo('Vencer a Thanos');
+const todo2 = new Todo('Salvar al mundo');
+const todo3 = new Todo('Pedir prestado el traje de Iron man');
+
+const estadoInicial: Todo[] = [todo1, todo2, todo3];
 
 export function todoReducer(state = estadoInicial, action: fromTodo.Acciones): Todo[] {
     switch (action.type) {
